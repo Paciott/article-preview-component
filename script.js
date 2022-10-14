2844e286d321a4863d2305e33da1523b80dddb19
andrea <pacittoandrea@gmail.com>
2022-10-14 19:16:39 +0200

    update HTML, CSS and  JS files

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const shareBtn = document.querySelector(".share");
+const user = document.querySelector(".user");
+const tooltipElements = document.querySelector(".tooltip-elements");
+const tooltip = document.querySelector(".tooltip");
+const arrow = document.querySelector(".arrow");
 
+shareBtn.addEventListener("click", () => {
+  user.classList.toggle("closed");
+  tooltipElements.classList.toggle("tooltip-closed");
+  tooltip.classList.toggle("tooltip-open");
+  shareBtn.classList.toggle("clicked");
+  arrow.classList.toggle("changed");
+});
